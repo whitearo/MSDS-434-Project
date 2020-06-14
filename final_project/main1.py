31d92aff11f74ce30e2b0d01b63a6fe5a64afee2
aron <whitearo@live.com>
2020-06-13 18:57:53 -0700

    test_appengine

---
+++ final_project/main1.py
@@ -0,0 +1,23 @@
+# !/usr/bin/env python
+# Copyright 2018 Google LLC
+# batch update bigquery from imported csv
+
+# [START gae_python37_app]
+from flask import Flask, jsonify, request
+#from google.cloud import bigquery
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    message = request.get_json().get('message', '')
+    return jsonify({'message': message})
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
